871227ee65f7301ba2cf17954a7677d95ef53406
Mayank Rai <149506720+MayankRai89@users.noreply.github.com>
2026-04-03 21:01:49 +0530

    Refactor closestSum method in Threesumclosest.java for improved logic and readability

--- Threesumclosest.java
+++ Threesumclosest.java
@@ -1,37 +1,49 @@
 import java.util.*;
+
 public class Threesumclosest {
-    public static int closestSum(int nums[],int target){
+    public static int closestSum(int nums[], int target) {
         Arrays.sort(nums);
-
-        int j = nums.length-1;
-        for(int i = 0; i<nums.length; i++){
-            while(i<j){
-                int currrentsum = nums[i]+nums[j-1]+nums[j];
-                if(Math.abs(target - currrentsum)< Math.abs(target-closestsum)){
-                    closestsum = currrentsum;
+        int ClosestSum = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int currentsum = nums[i] + nums[left] + nums[right];
+                if (Math.abs(target - currentsum) < Math.abs(target - ClosestSum)) {
+                    ClosestSum = currentsum;
                 }
-
+                if (currentsum < target) {
+                    left++;
+                } else if (currentsum > target) {
+                    right--;
+                } else {
+                    return currentsum;
+                }
             }
-        }
 
+        }
+        return ClosestSum;
 
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of array: ");
         int n = sc.nextInt();
-        int [] nums = new int[n];
+        int[] nums = new int[n];
         System.out.print("Enter target: ");
         int target = sc.nextInt();
 
         // Enter the elements in array
-        for(int i=0; i<n; i++){
+        System.out.print("Enter elements in array: ");
+        for (int i = 0; i < n; i++) {
             nums[i] = sc.nextInt();
         }
         // printing elements in an array
-        for(int i=0; i<n; i++){
-            System.out.print(nums[i]+" ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(nums[i] + " ");
         }
+        System.out.println();
         int closest = closestSum(nums, target);
         System.out.println(closest);
 
