6a49fe71a838928f8baacff5a5fbdd512ef7d59b
Mayank Rai <149506720+MayankRai89@users.noreply.github.com>
2026-06-10 22:52:06 +0530

    Add first_occurenceElement class with method to find first occurrence of a target in an array

--- first_occurenceElement.java
+++ first_occurenceElement.java
@@ -1,1 +1,19 @@
-import java.utils.*;
+
+public class first_occurenceElement {
+
+    public static void firstoccur(int arr[], int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                System.out.println(i);
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 2, 3, 4, 5 };
+        int target = 2;
+        firstoccur(arr, target);
+
+    }
+}
