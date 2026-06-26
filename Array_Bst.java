99ba7e59a5cd447b9d66b15029a2c705a7623672
Mayank Rai <149506720+MayankRai89@users.noreply.github.com>
2026-06-26 20:10:00 +0530

    Add TreeNode class and implement sortedArrayToBST method in Array_Bst class

--- Array_Bst.java
+++ Array_Bst.java
@@ -1,34 +1,67 @@
-import java.util.*;
-//  Definition for a binary tree node.
-//  public class TreeNode {
-//       int val;
-//       TreeNode left;
-//       TreeNode right;
-//       TreeNode() {}
-//       TreeNode(int val) { this.val = val; }
-//       TreeNode(int val, TreeNode left, TreeNode right) {
-//           this.val = val;
-//           this.left = left;
-//           this.right = right;
-//      }
-//   }
- 
-class Array_Bst {
-    public TreeNode sortedArrayToBST(int[] nums) {
-        if(nums.length  == 0){
-            return null;
-        }
-    return buildBst(nums, 0, nums.length-1);
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
     }
-    public TreeNode buildBst(int []nums, int left, int right){
-        if(left>right){
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Array_Bst {
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums == null || nums.length == 0) {
             return null;
         }
-        int mid = left +(right - left)/2;
-         TreeNode root = new TreeNode(nums[mid]);
-        root.left = buildBst(nums, left, mid-1);
-        root.right = buildBst(nums, mid+1, right);
+
+        return buildBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildBST(int[] nums, int left, int right) {
+
+        if (left > right) {
+            return null;
+        }
+
+        int mid = left + (right - left) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+
+        root.left = buildBST(nums, left, mid - 1);
+        root.right = buildBST(nums, mid + 1, right);
 
         return root;
+    }
+
+    // Inorder Traversal
+    public void inorder(TreeNode root) {
+        if (root == null)
+            return;
+
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        Array_Bst obj = new Array_Bst();
+
+        int[] nums = { -10, -3, 0, 5, 9 };
+
+        TreeNode root = obj.sortedArrayToBST(nums);
+
+        System.out.println("Inorder Traversal:");
+        obj.inorder(root);
     }
 }
