feca7e9fbb95fb16f9b8a7bd8a0ff9e1811a3f4e
Mayank Rai <149506720+MayankRai89@users.noreply.github.com>
2026-07-14 01:00:10 +0530

    Refactor insert method in DeleteNthnode class for improved readability and maintainability

--- LinkedsList/DeleteNthnode.java
+++ LinkedsList/DeleteNthnode.java
@@ -14,13 +14,18 @@ public class DeleteNthnode {
     // METHOD TO CREATE LINKEDLIST
     public void insert(int data) {
         Node newNode = new Node(data);
+
         if (head == null) {
             head = newNode;
+            return;
         }
+
         Node temp = head;
+
         while (temp.next != null) {
             temp = temp.next;
         }
+
         temp.next = newNode;
     }
 
@@ -36,5 +41,20 @@ public class DeleteNthnode {
             temp = temp.next;
         }
         System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        DeleteNthnode list = new DeleteNthnode();
+
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+        list.insert(50);
+        list.insert(60);
+        list.insert(70);
+        list.display();
+
     }
 }
